77b35f64663d7d7fcb3a4254087a8e4d5f70b8a7
Samuel <samuelreyesiglesias@gmail.com>
2023-11-15 14:11:42 -0600

    sockets

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
+session_start();
+if(!isset($_SESSION['count'])){
+    $_SESSION['count']=0;
+}
+else{
+    $_SESSION['count']++;
+}
 $address="127.0.0.1";
 $port="3222";
-$msg="Hello server";
+$msg="Hello server" . $_SESSION['count'];
 
 $sock=socket_create(AF_INET,SOCK_STREAM,0) or die("Cannot create a socket");
 socket_connect($sock,$address,$port) or die("Could not connect to the socket");
--- server.php
+++ server.php
@@ -12,26 +12,43 @@ socket_write($accept,"Hello client");
 socket_close($sock);
 */
 
-//script to keep the server running forever
+//script to keep the server running forever and add a counter to the message sent to the client in a session
+/* 
+session_start();
 $address="127.0.0.1";
 $port="3222";
 $sock=socket_create(AF_INET,SOCK_STREAM,0) or die("Cannot create a socket");
 socket_bind($sock,$address,$port) or die("Couldnot bind to socket");
 socket_listen($sock) or die("Couldnot listen to socket");
-$accept=socket_accept($sock) or die("Couldnot accept");
-$read=socket_read($accept,1024) or die("Cannot read from socket");
-echo $read;
-socket_write($accept,"Hello client");
-//socket_close($sock);
-//unclose to keep the server running forever
 while(true){
     $accept=socket_accept($sock) or die("Couldnot accept");
-    $read=socket_read($accept,1024) or die("Cannot read from socket");
+    $read=socket_read($accept,1024) or die("Cannot read from socket"); 
     echo $read;
-    socket_write($accept,"Hello client");
-    sleep(3);
+    $msg="Hello client" ;
+    socket_write($accept,$msg);
+    sleep(1);
+  //  socket_close($accept);
 }
+//socket_close($sock);
+*/ 
 
-//not needed if the server is running forever
-//socket_close($accept);
+session_start(); 
+$counter = 0; 
+$address="127.0.0.1"; 
+$port="3222"; 
+$sock=socket_create(AF_INET,SOCK_STREAM,0) or die("Cannot create a socket"); 
+socket_bind($sock,$address,$port) or die("Couldnot bind to socket"); 
+socket_listen($sock) or die("Couldnot listen to socket"); 
+while(true){ 
+    $accept=socket_accept($sock) or die("Couldnot accept"); 
+    $read=socket_read($accept,1024) or die("Cannot read from socket"); 
+    echo "Message ".++$counter." sent to client: ".$read; 
+    $msg="Hello client" . $counter ; 
+    socket_write($accept,$msg); 
+    //sleep(1); 
+}
+ 
+
+
+ 
 ?>
